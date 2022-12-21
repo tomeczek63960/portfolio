d56f99ca0dd81070abb842443ade61d32365ffd2
Tomasz <tomasz@MacBook-Pro-Tomasz.local>
2022-12-21 19:49:43 +0100

    update cv box style

---
+++ src/ui/Cv/CvBox/index.tsx
@@ -0,0 +1,34 @@
+import Pdf from '../../../../public/svg/pdf.svg';
+import { useScrollTrigger } from 'src/hooks/useScrollTrigger';
+import {
+  StyledCvCardWrapper,
+  StyedCvCard,
+  StyledCvCardHead,
+  StyledDownloadCvIcon,
+  StyledDownloadCvLink,
+  StyedCvCardContent,
+  StyledCardListItem,
+} from './style';
+
+const CvBoxComponent: React.FC = () => {
+  const [ul] = useScrollTrigger(0.6, 'children') as [React.RefObject<HTMLUListElement>];
+  const [box] = useScrollTrigger(0.6, 'children') as [React.RefObject<HTMLDivElement>];
+  return (
+    <StyledCvCardWrapper position="left">
+			<StyedCvCard position="left">
+				<StyledCvCardHead ref={box}>
+					<StyledDownloadCvIcon href="/cv.pdf"><Pdf /></StyledDownloadCvIcon>
+					<StyledDownloadCvLink href="/cv.pdf" download>Download CV</StyledDownloadCvLink>
+				</StyledCvCardHead>
+				<StyedCvCardContent position="left" ref={ul}>
+					<StyledCardListItem>Spodobały Ci się moje projekty</StyledCardListItem>
+					<StyledCardListItem>Mój stack technologiczny pasuje do twojej firmy/ogłoszenia</StyledCardListItem>
+					<StyledCardListItem>Oraz moje doświadczenie jest tym czego szukasz?</StyledCardListItem>
+					<StyledCardListItem>kliknij w link powyżej żeby pobrać moje Cv 😊.</StyledCardListItem>
+				</StyedCvCardContent>
+			</StyedCvCard>
+    </StyledCvCardWrapper>
+  )
+}
+
+export default CvBoxComponent;
--- src/ui/Cv/index.tsx
+++ src/ui/Cv/index.tsx
@@ -1,22 +1,11 @@
 import HeadingComponent from 'src/ui/Heading';
 import Paragraph from 'src/ui/Paragraph';
-import Pdf from '../../../public/svg/pdf.svg';
-import { useScrollTrigger } from 'src/hooks/useScrollTrigger';
 import {
   StyledCvSection,
-  StyledCvCardWrapper,
-  StyedCvCard,
-  StyledCvCardHead,
-  StyledDownloadCvIcon,
-  StyledDownloadCvLink,
-  StyedCvCardContent,
-  StyledCardListItem,
 } from './style';
+import CvBoxComponent from './CvBox';
 
 const CvComponent: React.FC = () => {
-  const [ul] = useScrollTrigger(0.5, 'children') as [React.RefObject<HTMLUListElement>];
-  const [box] = useScrollTrigger(0.5, 'children') as [React.RefObject<HTMLDivElement>];
-
   return (
     <StyledCvSection>
       <HeadingComponent tagName='h2' color="#6A82FB">
@@ -25,22 +14,9 @@ const CvComponent: React.FC = () => {
       <Paragraph>
         Doświadczenie zawodowe, moje aktualne umiejętności, opis komercyjnych projektów w których miałem okazję uczestniczyć oraz opis prywatnych projektów wraz z linkami do live demo i kodu źródłowego, to wszystko zawarte jest w moim cv.
       </Paragraph>
-      <StyledCvCardWrapper position="left">
-        <StyedCvCard position="left">
-          <StyledCvCardHead className="imgbx" ref={box}>
-            <StyledDownloadCvIcon className="icon" href="/cv.pdf"><Pdf className="pdf"/></StyledDownloadCvIcon>
-            <StyledDownloadCvLink href="/cv.pdf" download>Download CV</StyledDownloadCvLink>
-          </StyledCvCardHead>
-          <StyedCvCardContent position="left" ref={ul}>
-              <StyledCardListItem>Spodobały Ci się moje projekty</StyledCardListItem>
-              <StyledCardListItem>Mój stack technologiczny pasuje do twojej firmy/ogłoszenia</StyledCardListItem>
-              <StyledCardListItem>Oraz moje doświadczenie jest tym czego szukasz?</StyledCardListItem>
-              <StyledCardListItem>kliknij w link powyżej żeby pobrać moje Cv 😊.</StyledCardListItem>
-          </StyedCvCardContent>
-        </StyedCvCard>
-      </StyledCvCardWrapper>
+      <CvBoxComponent />
     </StyledCvSection>
   )
 }
 
-export default CvComponent
+export default CvComponent;
