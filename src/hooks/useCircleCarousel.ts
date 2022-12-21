9b0fe53f7e773f007fbfadc07676479badf1b818
Tomasz <55184773+tomeczek63960@users.noreply.github.com>
2022-12-21 17:39:52 +0100

    refactore circle carousel (#3)
    Co-authored-by: Tomasz <tomasz@MacBook-Pro-Tomasz.local>

--- src/hooks/useCircleCarousel.ts
+++ src/hooks/useCircleCarousel.ts
@@ -1,104 +1,166 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { gsap } from "gsap";
 import useIsomorphicLayoutEffect from "src/animation/useIsomorphicLayoutEffect";
 
-export const useCircleCarousel = (): [React.RefObject<HTMLDivElement>, React.RefObject<HTMLDivElement>, React.RefObject<HTMLDivElement>, any, any] => {
-  const [isCarouselInited, setisCarouselInited] = useState<any>(false);
+const getAngle = (dots: number, next: number, prev: number, step: number) => {
+  const half = dots / 2;
+  if (prev > dots) prev = dots - 1;
+  let inc; 
+
+  if (Math.abs(inc = next - prev) <= half) return step * inc;
+  if (Math.abs(inc = next - prev + dots) <= half) return step * inc;
+  if (Math.abs(inc = next - prev - dots) <= half) return step * inc;
+  return step * inc;
+};
+
+interface CarouselConfig {
+  slides: HTMLCollectionOf<HTMLElement> | never[],
+  activeSlide: number,
+  dots: HTMLCollectionOf<HTMLElement> | never[],
+  angle: number,
+  prevSlide: number,
+  step: number,
+  autoplayId: NodeJS.Timer | undefined,
+}
+
+export const useCircleCarousel = (speed: number = 800, autoplay: number = 4500): [
+  React.RefObject<HTMLDivElement>,
+  React.RefObject<HTMLDivElement>,
+  React.RefObject<HTMLDivElement>,
+  React.MouseEventHandler<HTMLDivElement>,
+  React.MouseEventHandler<HTMLDivElement>,
+  React.MouseEventHandler<HTMLDivElement>,
+] => {
   const carouselRef = useRef<HTMLDivElement>(null);
-  const carouselPaginationRef = useRef<HTMLDivElement>(null);
+  const pagination = useRef<HTMLDivElement>(null);
   const carouselTextRef = useRef<HTMLDivElement>(null);
-  const carousel = useRef<any>();
-  useIsomorphicLayoutEffect(() => {
-    setisCarouselInited(true);
+  const slideTimeline = useRef(gsap.timeline());  
+  const configObject = useRef<CarouselConfig>({
+    slides: [],
+    activeSlide: 0,
+    dots: [],
+    angle: 0,
+    prevSlide: 0,
+    step: 0,
+    autoplayId: undefined
+  });
+  
+  useIsomorphicLayoutEffect((): void => {
+    const dots = pagination.current?.children as HTMLCollectionOf<HTMLElement> | never[];
+    const slides = carouselTextRef.current?.children as HTMLCollectionOf<HTMLElement> | never[];
+    configObject.current = {
+      ...configObject.current,
+      slides: slides,
+      dots: dots,
+      step: -360 / (dots?.length || 0),
+    }
+
+    Array.from(dots).forEach((dot: HTMLElement, index: number) => {
+      dot.style.transform = `rotate(${360 / dots.length * index}deg)`;
+    });
+    if (pagination.current) pagination.current.style.transitionDuration = `${speed}ms`;
+    if (autoplay && !configObject.current.autoplayId) startAutoplay();
   }, []);
-  useIsomorphicLayoutEffect(() => {
-    if (!isCarouselInited) return;
-    carousel.current = {
-      node        : carouselRef.current,
-      slides      : Array.from(carouselTextRef.current ? carouselTextRef.current.children : []),
-      slidesN     : carouselTextRef.current ? carouselTextRef.current.children.length : 0,
-      pagination  : carouselPaginationRef.current,
-      pagTransf   : 'translate( -50%, -50% )',
-      dots        : Array.from(carouselPaginationRef.current ? carouselPaginationRef.current.children : []),
-      dotsN       : carouselPaginationRef.current ? carouselPaginationRef.current.children.length : 0,
-      step        : -360/(carouselPaginationRef.current ? carouselPaginationRef.current.children.length : 0),
-      angle       : 0,
-      activeN     : 0,
-      prevN       : 0,
-      speed       : 800,
-      autoplay    : 4500,
-      autoplayId  : null,
+
+  const startAutoplay = (): void => {
+    configObject.current.autoplayId = setInterval(() => {
+      if (typeof document !== 'undefined' && !document.hidden) {
+        setSlide(configObject.current.activeSlide + 1);
+      }
+    }, autoplay);
+  };
+  const stopAutoplay = (): void => {
+    clearInterval(configObject.current.autoplayId);
+  };
+  const getTransformAnimateObject = (type: string = 'in'): any => { // TODO: dodać typ zwracanego obiektu
+    let animationObject;
+    if(type === 'in') {
+      animationObject = {
+        duration: 0.3,
+        opacity: 1,
+        y: 0,
+        pointerEvents: 'all',
+        stagger: 0.2
+      }
+    } else {
+      animationObject = {
+        duration: 0.3,
+        opacity: 0,
+        pointerEvents: 'none',
+        y: '-40%',
+        stagger: 0.2
+      }
     }
-    carousel.current.pagination.style.transitionDuration = carousel.current.speed +'ms';
-    carousel.current.dots.forEach(handleDotsListeners);
-    if (carousel.current.autoplay) startAutoplay();
-  }, [isCarouselInited]);
-  const handleDotsListeners = (dot: any, index: any) => {
-    dot.addEventListener('click', () => setSlide(index));
-    dot.style.transform = 'rotate('+ 360 / carousel.current.dotsN * index + 'deg)';
+    return animationObject;
+  };
+  const getTransformSvgObject = (type: string = 'in'): any => { // TODO: dodać typ zwracanego obiektu
+    let animationObject;
+    if(type === 'out') {
+      animationObject = {
+        duration: 0.5,
+        scale: '1',
+        filter: 'grayscale(100%)',
+      }
+    } else {
+      animationObject = {
+        duration: 0.5,
+        scale: '1.5',
+        filter: 'grayscale(0%)',
+      }
+    }
+    return animationObject;
+  };
+
+  const getSlideIndex = (index: number): number => {
+    let activeIndex = index;
+    if (index < 0) {
+      activeIndex = configObject.current.slides.length - 1;
+    } else if (index >= configObject.current.slides.length) {
+      activeIndex = 0;
+    }
+    return activeIndex;
   }
-  const setSlide = function (slideN: any) {
-    const tl = gsap.timeline();
-    carousel.current.slides[carousel.current.activeN].classList.remove('active');
-    tl.to(carousel.current.slides[carousel.current.activeN].children, {
-      duration: 0.4,
-      opacity: 0,
-      pointerEvents: 'none',
-      y: '-40%',
-      stagger: 0.2
-    });
 
-    if (carousel.current.dots[carousel.current.activeN]) {
-      carousel.current.dots[carousel.current.activeN].classList.remove('active');
+  const findClickedElementIndex = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): number => {
+    let target = event.target as HTMLElement;
+    while (!target.dataset.carouselItem) {
+      if (!target.parentElement) break;
+      target = target.parentElement;
     }
-
-    carousel.current.prevN = carousel.current.activeN;
-    carousel.current.activeN = slideN;
-    if (carousel.current.activeN < 0) {
-      carousel.current.activeN = carousel.current.slidesN -1;
-    } else if (carousel.current.activeN >= carousel.current.slidesN) {
-      carousel.current.activeN = 0;
-    }
-
-    carousel.current.slides[carousel.current.activeN].classList.toggle('active');	
-    tl.to(carousel.current.slides[carousel.current.activeN].children, {
-      duration: 0.4,
-      opacity: 1,
-      y: 0,
-      pointerEvents: 'all',
-      stagger: 0.2
-    });
-    if (carousel.current.dots[carousel.current.activeN]) {
-      carousel.current.dots[carousel.current.activeN].classList.toggle('active');
-    }
-
-    rotate();
-  };
-  const getAngle = (dots: any, next: any, prev: any, step: any) => {
-    let inc, half = dots / 2;
-    if (prev > dots) prev = dots - 1;
-    if (Math.abs(inc = next - prev) <= half) return step * inc;
-    if (Math.abs(inc = next - prev + dots) <= half) return step * inc;
-    if (Math.abs(inc = next - prev - dots) <= half) return step * inc;
+    return Array.from(configObject.current.dots).findIndex((item: HTMLElement) => item === target);
   }
-  const rotate = () => {
-    if (carousel.current.activeN >= carousel.current.dotsN) return;
-    carousel.current.angle += getAngle(carousel.current.dotsN, carousel.current.activeN, carousel.current.prevN, carousel.current.step);
-    carousel.current.pagination.style.transform = carousel.current.pagTransf +'rotate('+ carousel.current.angle +'deg)';
+
+  const changeSlideAnimation = (index: number, classAction: 'add' | 'remove'): void => {
+    const slide = configObject.current.slides[index];
+    const dots = configObject.current.dots[index];
+    const svg = dots.querySelector('svg');
+    const isEnterAnimation = classAction === 'add';
+    slideTimeline.current.to(svg, getTransformSvgObject(isEnterAnimation ? 'in' : 'out'), 'start');
+    slideTimeline.current.to(slide.children, getTransformAnimateObject(isEnterAnimation ? 'in' : 'out'));
+  }
+
+  const setSlide = (event: React.MouseEvent<HTMLDivElement, MouseEvent> | number): void => {
+    const prev = configObject.current.activeSlide;
+    const index = typeof event === 'number' ? event : findClickedElementIndex(event);
+    const active = getSlideIndex(index);
+    
+    changeSlideAnimation(prev, 'remove');
+    changeSlideAnimation(active, 'add');
+
+    configObject.current.prevSlide = prev;
+    configObject.current.activeSlide = active;
+
+    if (!pagination.current) return;
+    configObject.current.angle += getAngle(configObject.current.dots.length, active, prev, configObject.current.step);
+    pagination.current.style.transform = `translate(-50%, -50%) rotate(${configObject.current.angle}deg)`;
   };
-  const startAutoplay = () => {
-    carousel.current.autoplayId = setInterval(() => {
-      setSlide(carousel.current.activeN + 1);
-    }, carousel.current.autoplay);
-  };
-  const stopAutoplay = () => {
-    clearInterval(carousel.current.autoplayId);
-  };
+
   return [
     carouselRef,
-    carouselPaginationRef,
+    pagination,
     carouselTextRef,
     startAutoplay,
-    stopAutoplay
+    stopAutoplay,
+    setSlide
   ];
 }
