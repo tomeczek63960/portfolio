62f6fdb2602e533aaab04a3ab0728cca46752e99
Tomasz <tomaszkardel@wp.pl>
2023-01-05 11:46:17 +0100

    add navigation links translations

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -18,7 +18,7 @@ const ExperiencePage: FC<IPage> = ({ page }) => {
     //   <Experience />
     // </Layout>
     <Layout title="Title" description="Description" url="here">
-      {page.PageContent.map((content: any) => (
+      {page?.PageContent.map((content: any) => (
         <ComponentContent content={content} key={content.id} />
       ))}
     </Layout>
